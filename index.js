060c059437779632c999d4250636ef37d5027dfa
Hayden Faulds <fauldsh@gmail.com>
2021-07-24 13:11:26 +0100

    use commonjs modules

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import { PSDB } from 'planetscale-node'
-import * as Client_MySQL2 from 'knex/lib/dialects/mysql2/index.js'
+const { PSDB } = require('planetscale-node')
+const Client_MySQL2 = require('knex/lib/dialects/mysql2/index.js')
 
 class Client_PSDB extends Client_MySQL2 {
   acquireRawConnection() {
